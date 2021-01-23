34a4d7b136ac19f4fb9afe4037f377ef4b3d7da3
Tetsuya Kaneko <tetsuya@users.noreply.github.com>
2021-01-23 21:34:22 +0900

    Replace Bulma -> Taildwind

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,4 @@
-import '../stylesheets/application'
+require("stylesheets/application.scss")
 
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
